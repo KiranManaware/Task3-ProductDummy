e9724fab300a90ce7de9beccf37b7c749131af1f
KiranManaware <kiranmanaware123@gamil.com>
2024-10-05 13:41:15 +0530

    adding th

--- main.js
+++ main.js
@@ -6,6 +6,23 @@ const getdata=async()=>{
     const response= await fetch("https://dummyjson.com/products");
     const data= await response.json();
     // console.log(data.products[0]);
+    let tr = document.createElement("tr");
+    let td1=document.createElement("th")
+    let td2=document.createElement("th")
+    let td3=document.createElement("th")
+    let td4=document.createElement("th")
+    let td5=document.createElement("th")
+    td1.innerText="Id";
+    td2.innerText="Title";
+    td3.innerText="Description";
+    td4.innerText="Category";
+    td5.innerText="Price";
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tr.appendChild(td5);
+    table.appendChild(tr);
     data.products.forEach((d) => {
         let tr = document.createElement("tr");
         let td1=document.createElement("td")
